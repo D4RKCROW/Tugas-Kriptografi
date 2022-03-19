8475e23780005bdeae1c942cfe0d71223703d82e
D4RKCROW <90462446+D4RKCROW@users.noreply.github.com>
2022-03-19 20:39:59 +0700

    Add files via upload

---
+++ caesar_cipher_dengan_modulo_ALI.py
@@ -0,0 +1,41 @@
+
+def caesar_enkripsi(kalimat_awal,kunci):
+  enkripsi = ''
+  for i in kalimat_awal:
+    if i.isupper():
+      rumus = 65 + ((ord(i) - 65 + kunci) % 26) 
+      enkripsi = enkripsi + chr(rumus)                              
+    elif i.islower():
+      rumus = 97 + ((ord(i) - 97 + kunci) % 26)
+      enkripsi = enkripsi + chr(rumus)
+    else:
+      enkripsi = enkripsi +  " "
+ 
+  print("Hasil enkripsinya adalah:",enkripsi)
+  
+def caesar_dekripsi(hasil_enkripsi,kunci):
+  dekripsi = ''
+  for i in hasil_enkripsi:
+    if i.isupper():
+        rumus = 65 + ((ord(i) - 65 - kunci) % 26) 
+        dekripsi = dekripsi + chr(rumus)                              
+    elif i.islower():
+        rumus = 97 + ((ord(i) - 97 - kunci) % 26) 
+        dekripsi = dekripsi + chr(rumus)
+    else:
+        dekripsi = dekripsi + " "  
+ 
+  print("Hasil dekripsi dari kalimat tersebut adalah : ",dekripsi)
+
+print("Masukkan pilihan")
+print("1. Enkripsi")
+print("2. Dekripsi")
+pilihan = int(input())
+if pilihan == 1:
+    kalimat_awal = input("Masukkan kalimat yang akan di enkripsi: ")
+    kunci = int(input("Masukkan kunci : "))
+    caesar_enkripsi(kalimat_awal,kunci)
+if pilihan == 2:
+    hasil_enkripsi = input("Masukkan kalimat yang akan di dekripsi : ")
+    kunci = int(input("Masukkan kunci : "))
+    caesar_dekripsi(hasil_enkripsi,kunci)
